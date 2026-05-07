abfce634d064912b787f5410e3bab004f5057c6e
Sergio <sergiosorbi@gmail.com>
2026-05-07 16:29:31 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import './App.css'
+import About from './pages/about/About';
 import Homepage from './pages/Homepage'
 import "@sergio/styleguide";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Projects from './pages/projects/Projects';
 
 function App() {
   return (
     <div className="mfe-maincontent">
       <div className="row">
         <div className="col col-12">
-          <main>
-            <Homepage />
+          <main className='main-content-routes'>
+            <BrowserRouter basename="/">
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+              </Routes>
+            </BrowserRouter>
           </main>
         </div>
       </div>
